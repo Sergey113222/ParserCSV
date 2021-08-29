910d3fc092415dbf299851275465fdd442c12e0b
Gritsuk_Sergey <k-grine@mail.ru>
2021-08-29 21:23:46 +0300

    29.08.21 add tests

--- src/test/java/com/example/parser/service/impl/ParserServiceImplTest.java
+++ src/test/java/com/example/parser/service/impl/ParserServiceImplTest.java
@@ -1,46 +1,25 @@
 package com.example.parser.service.impl;
 
-import com.example.parser.dto.RequestDto;
-import com.example.parser.model.ParserModel;
 import com.example.parser.repository.ParserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class ParserServiceImplTest {
     private ParserServiceImpl parserService;
     private Selector selector;
     private ParserRepository parserRepository;
-    private ParserModel parserModel;
     private Set<String> urls;
-    private List<ParserModel> parserModelList;
-    private RequestDto requestDto;
 
     private final String str = "Tesla Model 3 and Tesla Model S";
     private final String url = "https://en.wikipedia.org/wiki/Elon_Musk";
-
-    {
-        requestDto = new RequestDto();
-        requestDto.setUrl(url);
-    }
-
-    {
-        parserModel = new ParserModel();
-
-        parserModel.setUrl("https://en.wikipedia.org/wiki/Elon_Musk");
-        parserModel.setTesla(1);
-        parserModel.setMusk(2);
-        parserModel.setGigafactory(3);
-        parserModel.setElonMusk(4);
-        parserModel.setTotal(10);
-        parserModelList = new ArrayList<>();
-        parserModelList.add(parserModel);
-    }
 
     {
         urls = new HashSet<>();
